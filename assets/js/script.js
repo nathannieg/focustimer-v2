a2025ef781ed16e5584c3b634c5223e9d814bbd2
Nathannie Gomes <nathannieg@gmail.com>
2023-02-24 07:23:11 -0300

    adding event listener to control buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,35 @@ const buttonPlay = document.querySelector('.controls__play')
 const buttonPause = document.querySelector('.controls__pause')
 const buttonStop = document.querySelector('.controls__stop')
 const buttonSet = document.querySelector('.controls__set')
+const buttonSoundOn = document.querySelector('.sound--on')
+const butttonSoundOff = document.querySelector('.sound--off')
 
-// Play e pause
 buttonPlay.addEventListener('click', () => {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
+
+  buttonSet.classList.add('hide')
+  buttonStop.classList.remove('hide')
 })
 
 buttonPause.addEventListener('click', () => {
   buttonPause.classList.add('hide')
   buttonPlay.classList.remove('hide')
+})
+
+buttonStop.addEventListener('click', () => {
+  buttonStop.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonPlay.classList.remove('hide')
+})
+
+buttonSoundOn.addEventListener('click', () => {
+  buttonSoundOn.classList.add('hide')
+  butttonSoundOff.classList.remove('hide')
+})
+
+butttonSoundOff.addEventListener('click', () => {
+  butttonSoundOff.classList.add('hide')
+  buttonSoundOn.classList.remove('hide')
 })
